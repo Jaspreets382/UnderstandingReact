6af8aa54ca95ed80228e4351d6d1e8e55684a21d
jaspreet singh <js480292@gmail.com>
2025-09-30 09:53:39 +0530

    learned about redux implementation

---
+++ redux todo/src/features/todo/todoSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const TodoSlice=createSlice({
+    name:'todo',
+    initialState:{
+        todos:[{
+            id:1,
+            text:''
+        }]
+    },
+    reducers:{
+        addTodo: (state,action)=>{
+            const todo={
+                id:nanoid(),
+                text:action.payload
+            }
+            if(todo.text) state.todos.push(todo)
+        },
+        removeTodo:(state,action)=>{
+            state.todos=state.todos.filter((todo)=>todo.id!==action.payload)
+        },
+        updateTodo:(state,action)=>{
+           const {id,text}=action.payload
+          const todo= state.todos.find((todo)=>todo.id===id)
+          if(todo) todo.text=text
+        },
+
+    }
+})
+export const {addTodo,removeTodo,updateTodo} =TodoSlice.actions
+
+export const TodoReducer= TodoSlice.reducer
